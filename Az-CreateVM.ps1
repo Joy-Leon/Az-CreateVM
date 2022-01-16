b6582a864fc590770cb2de38b1bb856cbe5619a7
Joy-Leon <67964202+Joy-Leon@users.noreply.github.com>
2022-01-16 13:11:58 +0100

    Create Az-CreateVM.ps1

---
+++ Az-CreateVM.ps1
@@ -0,0 +1,95 @@
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+#          _             _                _            _            _   _         _                 _          _          _             _         #
+#         / /\          /\_\             /\ \         /\ \         /\_\/\_\ _    / /\              /\ \       /\ \       /\ \     _    /\ \       #
+#        / /  \        / / /         _   \_\ \       /  \ \       / / / / //\_\ / /  \             \_\ \      \ \ \     /  \ \   /\_\ /  \ \      #
+#       / / /\ \       \ \ \__      /\_\ /\__ \     / /\ \ \     /\ \/ \ \/ / // / /\ \            /\__ \     /\ \_\   / /\ \ \_/ / // /\ \_\     #
+#      / / /\ \ \       \ \___\    / / // /_ \ \   / / /\ \ \   /  \____\__/ // / /\ \ \          / /_ \ \   / /\/_/  / / /\ \___/ // / /\/_/     #
+#     / / /  \ \ \       \__  /   / / // / /\ \ \ / / /  \ \_\ / /\/________// / /  \ \ \        / / /\ \ \ / / /    / / /  \/____// / / ______   #
+#    / / /___/ /\ \      / / /   / / // / /  \/_// / /   / / // / /\/_// / // / /___/ /\ \      / / /  \/_// / /    / / /    / / // / / /\_____\  #
+#   / / /_____/ /\ \    / / /   / / // / /      / / /   / / // / /    / / // / /_____/ /\ \    / / /      / / /    / / /    / / // / /  \/____ /  #
+#  / /_________/\ \ \  / / /___/ / // / /      / / /___/ / // / /    / / // /_________/\ \ \  / / /   ___/ / /__  / / /    / / // / /_____/ / /   #
+# / / /_       __\ \_\/ / /____\/ //_/ /      / / /____\/ / \/_/    / / // / /_       __\ \_\/_/ /   /\__\/_/___\/ / /    / / // / /______\/ /    #
+# \_\___\     /____/_/\/_________/ \_\/       \/_________/          \/_/ \_\___\     /____/_/\_\/    \/_________/\/_/     \/_/ \/___________/     #
+#                            _              _        _            _       _                _        _    _        _                               #
+#                           / /\      _    /\ \     /\ \         / /\    / /\             /\ \     /\ \ /\ \     /\_\                             #
+#                          / / /    / /\   \ \ \    \_\ \       / / /   / / /             \ \ \   /  \ \\ \ \   / / /                             #
+#                         / / /    / / /   /\ \_\   /\__ \     / /_/   / / /              /\ \_\ / /\ \ \\ \ \_/ / /                              #
+#                        / / /_   / / /   / /\/_/  / /_ \ \   / /\ \__/ / /              / /\/_// / /\ \ \\ \___/ /                               #
+#                       / /_//_/\/ / /   / / /    / / /\ \ \ / /\ \___\/ /      _       / / /  / / /  \ \_\\ \ \_/                                #
+#                      / _______/\/ /   / / /    / / /  \/_// / /\/___/ /      /\ \    / / /  / / /   / / / \ \ \                                 #
+#                     / /  \____\  /   / / /    / / /      / / /   / / /       \ \_\  / / /  / / /   / / /   \ \ \                                #
+#                    /_/ /\ \ /\ \/___/ / /__  / / /      / / /   / / /        / / /_/ / /  / / /___/ / /     \ \ \                               #
+#                    \_\//_/ /_/ //\__\/_/___\/_/ /      / / /   / / /        / / /__\/ /  / / /____\/ /       \ \_\                              #
+#                        \_\/\_\/ \/_________/\_\/       \/_/    \/_/         \/_______/   \/_________/         \/_/                              #
+#                                                                                                                                                 #
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+# Disclaimer:                                                                                                                                     #
+#                                                                                                                                                 #
+# This script comes with no guarantees. The cmdlets in this script functioned as is on the moment of creating the script.                         #
+# It is possible that during the lifecycle of the product this script is intended for, updates were performed to the systems and the script       #
+# might not, or might to some extent, no longer function.                                                                                         #
+#                                                                                                                                                 #
+# Therefor, I would suggest running the script in a test environment first, cmdlet per cmdlet, before effectively running it in production        #
+# environments.                                                                                                                                   #
+#                                                                                                                                                 #
+# Created by Leon Moris                                                                                                                           #
+# Website: www.switchtojoy.be                                                                                                                     #
+# Github: https://github.com/Joy-Leon                                                                                                             #
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# Declared functions.
+function func_logging {   
+    param ($String) 
+    func_writeok $string
+    return "[{0:dd/MM/yy} {0:HH:mm:ss}] $String" -f (Get-Date)  | Out-File $logfile -append
+}
+function func_writeok {
+    param ($string)
+    write-host $string -f green
+}
+function func_writenok {
+    param ($string)
+    write-host $string -f red
+}
+
+# Declared variables.
+$logfile = Get-Location
+$logfile = "$logfile\logfile.txt"
+if (Test-Path $logfile) {
+    if (Test-Path "$logfile.old") {
+        Remove-Item "$logfile.old"
+    }
+    move-item $logfile -destination "$logfile.old"
+}
+
+# Make sure you have the AZ Powershell Module installed.
+$Subscription = "00000000-0000-0000-0000-000000000000"
+
+# Make sure to either enter your Resource Group or edit the name and location to your specific needs.
+$AzResourceGroup = $null
+$AzResourceGroupName = "AutomatingWithJoy"
+$AzResourceGroupLocation = "West Europe"
+
+# Make sure to edit the VM variables to your own needs.
+$AzVMName = "JoyVM-WeEur-01"
+$AzVMImage = "UbuntuLTS"
+
+# Connect to your Azure Tenant Account.
+Connect-AzAccount
+
+# Change to the subscription of your choice on where to create the new VM.
+Set-AZContext -Subscription $Subscription | Out-File $logfile -append
+func_writeok "The subscription has been set to $Subscription"
+
+# Check if there is an Resource group available or not, and either create it or not.
+if ($AzResourceGroup -eq $null) {
+New-AzResourceGroup -Name $AzResourceGroupName -Location $AzResourceGroupLocation | Out-File $logfile -append
+func_writeok "A new AzResourceGroup has been created with the name $AzResourceGroupName in the location $AzResourceGroupLocation"
+}
+Else {
+func_writeok "The AzResourceGroup variable was assigned a value: $AzResourceGroup"
+}
+
+# Create the VM with the needed variables.
+New-AzVm -ResourceGroupName $AzResourceGroup -Name $AzVMName -Credential (Get-Credential) -Location $AzResourceGroupLocation -Image UbuntuLTS | Out-File $logfile -append
+func_writeok "The AzVM has been created in the resource group $AzResourceGroup with the name $AzVMName in the location $AzResourceGroupLocation
